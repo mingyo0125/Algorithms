a3746dc9bc5843a99a14c747574712b8a12498f1
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-07-24 14:44:19 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string input;
+
+	while (true)
+	{
+		getline(cin, input);
+		if (input == "")
+		{
+			break;
+		}
+
+		cout << input << '\n';
+	}
+
+}
+
