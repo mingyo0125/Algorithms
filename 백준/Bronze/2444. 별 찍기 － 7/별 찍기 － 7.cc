3cfa9f43f37201d6f1bcfd0d9cccca293c785a60
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-07-25 13:57:39 +0900

    Update 별 찍기 － 7.cc

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -7,32 +7,32 @@ int main()
 
 	cin >> input;
 
-	for (int i = 1; i <= input; i++)
+	for (int i = 1; i <= input; i++)					// 몇단 삼각형인지
 	{
-		for (int j = 0; j < input - i; j++)
+		for (int j = 0; j < input - i; j++)				// 삼각형 앞에 오는 공백 출력
 		{
 			cout << " ";
 		}
 
-		for (int k = 1; k <= 2 * i - 1; k++)
+		for (int k = 1; k <= 2 * i - 1; k++)			// 삼각형을 이루는 별 출력
 		{
 			cout << "*";
 		}
+
 		cout << "\n";
 	}
 
-	for (int i = 1; i <= input - 1; i++)
+	for (int i = 1; i <= input; i++)				// 몇단 삼각형인지(역삼각형)
 	{
-		for (int j = 1; j <= i; j++)
+		for (int j = 1; j <= i; j++)					// 삼각형 앞에 오는 공백 출력(역삼각형)
 		{
 			cout << " ";
 		}
 
-		for (int k = (input - i) * 2 - 1; k > 0; k--)
+		for (int k = (input - i) * 2 - 1; k > 0; k--)   // 삼각형을 이루는 별 출력
 		{
 			cout << "*";
 		}
-
 
 		cout << "\n";
 	}
