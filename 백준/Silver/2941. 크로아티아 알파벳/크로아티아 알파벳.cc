62f230fe97614fb0fc17920116ea1418d8e702d7
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-07-27 16:20:01 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string croatiaAlpha[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+    string input;
+    cin >> input;
+
+    int cnt = 0;
+    int pos = 0;
+
+    while (pos < input.length())
+    {
+        bool isCroatianLetter = false;
+        for (string curAlpha : croatiaAlpha)
+        {
+            if (input.substr(pos, curAlpha.length()) == curAlpha)
+            {
+                cnt++;
+                pos += curAlpha.length();
+                isCroatianLetter = true;
+                break;
+            }
+        }
+
+        if (!isCroatianLetter)
+        {
+            cnt++;
+            pos++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
