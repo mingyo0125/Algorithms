f1c7a9e3dc59d3c522644401ae2391216b1d0b87
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-07-28 12:51:02 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int inputNum = 0;
+
+    cin >> inputNum;
+
+    int cnt = 0;
+
+    for (int i = 0; i < inputNum; i++)
+    {
+        string input;
+
+        cin >> input;
+
+        bool isGroup = true;
+
+        for (int j = 0; j < input.length(); j++)
+        {
+            for (int k = j + 2; k < input.length(); k++)
+            {
+                if (input[j] == input[k])
+                {
+                    if (input[j] == input[j + 1])
+                    {
+                        break;
+                    }
+                    isGroup = false;
+                    break;
+                }
+            }
+        }
+
+        if (isGroup)
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}
