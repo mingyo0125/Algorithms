686918caa97f71fa1ec7767163190b003e529d99
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-07-31 16:52:11 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	char arr[5][16] = {0};
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (arr[j][i] != NULL)
+			{
+				cout << arr[j][i];
+			}
+		}
+	}
+}
