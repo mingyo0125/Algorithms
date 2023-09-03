8d69d4a5fa1f426b35f8d0a6f7782f092d34fb73
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-09-03 18:09:04 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+ using namespace std;
+
+ int main()
+ {
+	 int n;
+	 int cnt = 0;
+	 cin >> n;
+
+	 while (n >= 0)
+	 {
+		 if (n % 5 == 0)
+		 {	
+			 cnt += (n / 5);
+			 cout << cnt << endl;
+			 return 0;
+		 }
+		 n -= 3; 
+		 cnt++;
+	 }
+	 cout << -1;
+ }
