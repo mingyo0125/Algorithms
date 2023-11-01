9d436b98e449bb523bb7812e10c08b0b9d65ca29
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-11-01 16:41:32 +0900

    [Silver III] Title: N과 M (3), Time: 960 ms, Memory: 92048 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int n, m;
+vector<int> vec;
+set<vector<int>> _set;
+
+void Backtrack(int x)
+{
+    if (vec.size() == m)
+    {
+        _set.insert(vec);
+        return;
+    }
+
+    for (int i = x; i <= n; i++)
+    {
+        vec.push_back(i);
+        Backtrack(1);
+        vec.pop_back();
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    Backtrack(1);
+
+    for (auto v : _set)
+    {
+        for (auto i : v)
+        {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+}
