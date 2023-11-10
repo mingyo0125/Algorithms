e24024474730b07570b62915e8377ef25f5c68de
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-11-10 11:52:01 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,29 +1,36 @@
-#include <iostream>
+#include <string>
+#include <set>
 #include <map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	map<int, int> _map;
+    int n, m, input;
+    vector<int> vec;
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int input;
-		cin >> input;
-		_map[input]++;
-	}
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input;
 
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; i++)
-	{
-		int input;
-		cin >> input;
-		cout << _map[input] << " ";
-	}
+        vec.push_back(input);
+    }
+    sort(vec.begin(), vec.end());
+    
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> input;
+        vector<int>::iterator lo = lower_bound(vec.begin(), vec.end(), input);
+        vector<int>::iterator up = upper_bound(vec.begin(), vec.end(), input);
+        int index = (up - vec.begin()) - (lo - vec.begin());
+        cout << index << " ";
+    }
 }
