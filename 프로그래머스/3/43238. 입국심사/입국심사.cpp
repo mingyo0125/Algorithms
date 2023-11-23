1dd604f401397632ce0a035c7ddaefe819ba3f3a
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-11-24 08:54:48 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -2,34 +2,29 @@
 #include <vector>
 #include <algorithm>
 #define longlong long long
+
 using namespace std;
 
-long long solution(int n, vector<int> times)
-{
-	sort(times.begin(), times.end());
+longlong solution(int n, vector<int> times) {
+    sort(times.begin(), times.end());
+    longlong left = times.front(), right = (longlong)times.front() * n; // 탐색 범위 정하기
+    longlong time, people;
+    while (left <= right)
+    {
+        people = 0;
+        time = (left + right) / 2;
+        for (auto t : times)
+            people += time / t; // 주어진 시간에서 채울 수있는 사람 수 구하기
 
-	longlong left = times.front(), right = (long long)times.front() * n; //가장 적게 걸리는 한 사람이 모두 하는 경우
-	longlong time, people;
-	while (left <= right)
-	{
-		people = 0;
-		time = (left + right) / 2;
-		for(auto t: times)
-		{
-			people += time / t;
-		}
-
-		if (people >= n)
-		{
-			right = time - 1;
-		}
-		else
-		{
-			left = time + 1;
-		}
-	}
-	return left;
-}
-int main()
-{
+        if (people >= n)
+        {
+            right = time - 1;
+        }
+        else
+        {
+            left = time + 1;
+        }
+        // 주어진 시간에서 n명을 채우기 어렵다면
+    }
+    return left;
 }
