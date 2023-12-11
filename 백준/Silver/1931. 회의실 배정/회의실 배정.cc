17523756f902ee97c0d3a57040d0797a86b01f34
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2023-12-11 11:45:40 +0900

    [Silver I] Title: 회의실 배정, Time: 96 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define longlong long long
+
+int main()
+{
+    vector<pair<int, int>> vec;
+    pair<int, int> cur;
+
+    int n, start, end, cnt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> start >> end;
+
+        vec.push_back(make_pair(end,start));
+    }
+
+    sort(vec.begin(), vec.end());
+
+    //cur = vec[0];
+    //cnt++;
+
+    for (auto v : vec)
+    {
+        if (cur.first <= v.second)
+        {
+            cur = v;
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}
