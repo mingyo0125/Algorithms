572e6085269fc9c483216abd64ebece7bc664458
mingyo0125 <98925706+mingyo0125@users.noreply.github.com>
2025-03-19 16:26:43 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ Python3/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,11 +1,11 @@
 def solution(price):
     if price >= 500_000:
-        discount_rate = 0.2
+        discountValue = 0.2
     elif price >= 300_000:
-        discount_rate = 0.1
+        discountValue = 0.1
     elif price >= 100_000:
-        discount_rate = 0.05
+        discountValue = 0.05
     else:
-        discount_rate = 0
+        discountValue = 0
 
-    return int(price * (1 - discount_rate))
+    return int(price * (1 - discountValue))
